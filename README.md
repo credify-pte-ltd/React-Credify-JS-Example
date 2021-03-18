c5df741344b7b2c6025292dca21663a4b7ed6b7b
ngo275 <ngo38114@gmail.com>
2021-03-18 23:08:22 +0700

    Updated the deps

--- README.md
+++ README.md
@@ -1,19 +1,91 @@
-# Credify JS Example (React.js)
+# Credify Web Example (React.js)
 
-This project is an example project of Credify JS for React.js. This application is using a [backend example project](https://github.com/credify-pte-ltd/Nodejs-Credify-JS-Example).
+This project is an example project of Credify Web for React.js.
 
 ## How to use
-
-*NOTE: This project is using [Nodejs Example Project](https://github.com/credify-pte-ltd/Nodejs-Credify-JS-Example), so please clone this Nodejs project as well.*
 
 ```shell script
 # Install dependencies
 $ yarn
+
 # Run a project
 $ yarn start
 ```
 
-
-The port needs to be 3000.
-
 Hardcoded app config (`App.tsx`) is already registered as an OpenID Connect client in Credify sandbox system.
+
+
+## How to use Credify Web
+
+```shell script
+$ yarn add credify-web
+$ yarn add credify-web-react
+```
+
+First, you need to call `defineCustomElements()` in your application.
+
+```javascript
+/// index.ts or index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+// Additional lines
+import { applyPolyfills, defineCustomElements } from 'credify-web/loader';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
+
+// Additional lines
+applyPolyfills().then(() => {
+  defineCustomElements();
+});
+```
+
+You need to pass user information like following.
+
+```javascript
+const offer = document.getElementById("credify");
+offer.setUser(
+  `${data.id}`,
+  data.phoneNumber,
+  data.phoneCountryCode,
+  data.email,
+  data.firstName,
+  data.lastName,
+  data.credifyId
+);
+```
+
+You can display the offer like following.
+
+```javascript
+const handleClick = async () => {
+  const offer = document.getElementById("credify");
+  const list = await offer.getOffersList();
+  console.log(list);
+  offer.show();
+};
+```
+
+The follwoing is an example of CredifyOffer element.
+
+This configuration is set in the web console.
+
+```javascript
+const cnf = {
+  orgId: "b09e8f99-6d89-4e7d-83ea-a43a1787b3e0",
+  apiKey:
+    "WaXSjOqK0JqSOH1VJ6Op1kkQTdPAhffv5bflck7SzwRjCK0MqUmjSHyvfAan3djf",
+  getUserAPI: `https://dev-demo-api.credify.one/sendo/demo-user`,
+  createUrl: `https://dev-demo-api.credify.one/sendo/create`,
+};
+```
+
+```javascript
+// JSX
+<CredifyOffer id="credify" organizationId={cnf.orgId} apiKey={cnf.apiKey} env="development"
+createUrl={cnf.createUrl} />
+```
--- src/App.tsx
+++ src/App.tsx
@@ -3,30 +3,64 @@ import logo from './logo.svg';
 import './App.css';
 import './InputField.css';
 import ReactJson from "react-json-view";
+import { CredifyOffer } from "credify-web-react";
+
 
 // TEST APP configuration
 const api = "http://localhost:8000";
 
+const cnf = {
+  orgId: "b09e8f99-6d89-4e7d-83ea-a43a1787b3e0",
+  apiKey:
+    "WaXSjOqK0JqSOH1VJ6Op1kkQTdPAhffv5bflck7SzwRjCK0MqUmjSHyvfAan3djf",
+  getUserAPI: `https://dev-demo-api.credify.one/sendo/demo-user`,
+  createUrl: `https://dev-demo-api.credify.one/sendo/create`,
+};
+
 function App() {
+  const [user, setUser] = useState(null);
 
   const handleClick = async () => {
-    const q = phoneNumber ? `?phone_number=${phoneNumber}` : "";
-    const res = await fetch(`${api}/oidc${q}`);
+    const offer = document.getElementById("credify");
     // @ts-ignore
-    const r = await res.json();
-    console.log(r.url)
-    window.open(r.url);
+    const list = await offer.getOffersList();
+    console.log(list);
+    // @ts-ignore
+    offer.show();
   };
 
-  // @ts-ignore
-  const handleTextChange = (event) => {
-    const value = event.target.value;
-    console.log(value);
-    setPhoneNumber(value);
-  }
-
-  const [phoneNumber, setPhoneNumber] = useState("");
   const [claims, setClaims] = useState(null);
+
+  useEffect(() => {
+    const search = window.location.search;
+    let id;
+    let api = cnf.getUserAPI;
+    if (search.includes("id=")) {
+      id = search.split("id=")[1];
+    }
+    if (id) {
+      api = `${api}?id=${id}`;
+    }
+    fetch(api)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        const offer = document.getElementById("credify");
+        // @ts-ignore
+        offer.setUser(
+          `${data.id}`,
+          data.phoneNumber,
+          data.phoneCountryCode,
+          data.email,
+          data.firstName,
+          data.lastName,
+          data.credifyId
+        );
+        setUser(data);
+      });
+  }, []);
 
   useEffect(() => {
     const match = /access_token=([-\.\w]+)&?/g.exec(window.location.hash);
@@ -54,18 +88,18 @@ function App() {
 
   return (
     <div className="App">
+      <CredifyOffer
+        id="credify"
+        organizationId={cnf.orgId}
+        apiKey={cnf.apiKey}
+        env="development"
+        createUrl={cnf.createUrl}
+      />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
 
-        <div className="group">
-          <input type="text" name="phoneNumber" onChange={handleTextChange} value={phoneNumber} placeholder="+843812345678" />
-          <span className="highlight" />
-          <span className="bar" />
-          <label>Phone number (optional)</label>
-        </div>
-
         <button className="App-link" onClick={handleClick}>
-          Login with Credify
+          Open a random offer - serviceX
         </button>
         {claims && (
           <div className="Json-wrapper">
